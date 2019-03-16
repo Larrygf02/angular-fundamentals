c65f9c526f74e54dfc569adcf7266d1f834cb192
larrygf02 <raulgf_02@hotmail.com>
2019-03-16 10:45:12 -0500

    Configurando rutas en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent } 
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    FooterComponent
+    FooterComponent,
+    EncabezadoComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
